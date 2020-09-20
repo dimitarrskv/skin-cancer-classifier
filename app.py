bb6644dd3a8bb3191fbab0550c691344bd3834c9
Dimitar <dimitar.rskv@gmail.com>
2020-09-20 22:07:15 +0200

    init: classify sample image

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from fastai import *
+from fastai.vision.all import *
+
+app = Flask(__name__)
+
+# Set path
+path = Path()
+path.ls(file_exts='.pkl')
+path
+
+# Load model
+model = load_learner('./model.pkl')
+
+@app.route('/')
+def index():
+    return 'hello, world'
+
+@app.route('/classify', methods=['POST'])
+def predict():
+    prediction = model.predict('m_example.jpg')
+    response = {
+        "prediction": prediction[0]
+    }
+    return response
